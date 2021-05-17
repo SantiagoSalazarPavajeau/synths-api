f94e13ef320cfa7458a6f95645ce8cf1de45529b
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-05-17 18:05:54 -0400

    add mysql db and fix jpa

--- src/test/java/com/ssp/synths/SynthsApplicationTests.java
+++ src/test/java/com/ssp/synths/SynthsApplicationTests.java
@@ -2,8 +2,10 @@ package com.ssp.synths;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations= "classpath:application-test.properties")
 class SynthsApplicationTests {
 
 //	@Test
