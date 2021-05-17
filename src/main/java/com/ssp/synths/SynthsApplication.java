c4fdee381bcd234ec1ef47625949289badf34b5d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-05-17 11:35:06 -0400

    Initializr

---
+++ src/main/java/com/ssp/synths/SynthsApplication.java
@@ -0,0 +1,13 @@
+package com.ssp.synths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssp/synths/SynthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp.synths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
